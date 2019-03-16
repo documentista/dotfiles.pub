09fc860d9118eb0767026ed5f1c7f29e5e35d2a9
Matt Oquist <moquist@majen.net>
2019-03-15 23:35:41 -0400

    Remove .abcde.conf

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -73,3 +73,17 @@
 
 (add-to-list 'custom-theme-load-path "~/.solarized")
 (load-theme 'solarized t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   (quote
+    (paredit better-defaults clojure-mode-extra-font-locking clojure-mode ido-ubiquitous smex pkg-info dash evil))))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
