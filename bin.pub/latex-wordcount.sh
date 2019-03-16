146365ba10dea212b6ccb913e72ad03897c898fb
Matt Oquist <moquist@majen.net>
2019-03-16 00:39:55 -0400

    Remove unused scripts in bin.pub.

--- bin.pub/latex-wordcount.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# texcount.pl is here: http://app.uio.no/ifi/texcount/download.html
-
-wc=$(texcount.pl "$1" | grep 'Words in text' | awk '{print $NF}')
-sed --in-place=noidnoid "s/^Word count:.*\$/Word count: $wc/" "$1"
