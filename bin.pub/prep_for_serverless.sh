87a236127076ca68b9683b9b17b8dc549e874bef
Tom Dolan <tgdolan@gmail.com>
2020-08-05 02:17:32 -0400

    serverless setup: Utilize brew for installing serverless tools.

--- bin.pub/prep_for_serverless.sh
+++ bin.pub/prep_for_serverless.sh
@@ -1,59 +1,30 @@
-# verify env
-function pre-req-ok {
-    local -n ref=$1
-    if [[ `$2` == "$3" ]];
-    then
-      ref="true"
-    else
-      ref="false"
-    fi
-}
+#!/usr/bin/env zsh
 
-pre-req-ok NODE_OK "node -v" "v12.18.3"
-pre-req-ok YARN_OK "yarn -v" "1.22.4"
-pre-req-ok 
-
-#if [[ `node -v` == "v12.18.3" ]];
-#then
-#  NODE_OK="true"
-#else
-#  NODE_OK="false"
-#fi
-
-echo "node ok: $NODE_OK"
+# Script to install utilities to be used for Homeslice serverless development:
+# node
+# yarn
+# aws-cdk
+# docker
+# python
+# aws-cli
+# aws-sam-cli
+# Also, updated PATH so that python3.7 is at the beginning
 
 # remove node
-#sudo rm -rf /usr/local/bin/npm /usr/local/share/man/man1/node* /usr/local/lib/dtrace/node.d ~/.npm ~/.node-gyp 
+rm -rf /usr/local/bin/npm /usr/local/share/man/man1/node* /usr/local/lib/dtrace/node.d ~/.npm ~/.node-gyp 
 
-# install node 12 lts
-#brew install node@12
-#echo 'export PATH="/usr/local/opt/node@12/bin:$PATH"' >> ~/.bash_profile
-#source ~/.bash_profile
+# brew it up! install all the things; tapping where necessary
+brew tap aws/tap
+brew install node@12 yarn aws-cdk python awscli aws-sam-cli
 
-# install yarn globally
-#npm install -g yarn
+# OK, now let's update path: first in line python, then node
+echo 'export PATH="/usr/local/opt/node@12/bin:$PATH"' >> ~/.bash_profile
+echo "export SAM=`which python3`" >> ~/.bash_profile
+echo "export PATH=$SAM/bin:$PATH" >> ~/.bash_profile
 
-# install cdk globally
-#npm install -g aws-cdk
+# Lastly, set up AWS creds for A-account
+curl https://repo.forge.lmig.com/content/repositories/ets-releases/com/lmig/ets/public-cloud/aws-saml-api-keys/2.4-55/aws-saml-api-keys-2.4-55.zip --output aws-saml-api-keys-2.4-55.zip
 
-# install python
-#brew install python
-
-# docker install (we don't need
-
-# install aws-cli
-# curl https://awscli.amazonaws.com/AWSCLIV2.pkg --output AWSCLIV2.pkg
-# sudo installer -pkg ./AWSCLIV2.pkg -target /
-
-# install aws sam cli (this is a big install; for me python3.8 got installed
-# into /usr/local/Cellar/python@3.8/3.8.5
-# brew tap aws/tap
-# brew install aws-sam-cli
-
-# set up env for SAM (essentially, adding python3 to path )
-# which python3 | xargs echo >> ~/.bash_profile
-# echo "export SAM;" >> ~/.bash_profile
-# echo "export PATH=$SAM/bin:$PATH" >> ~/.bash_profile
-
-
+unzip aws-saml-api-keys-2.4-55.zip
+open apikeyretriever_darwin_amd64
 
