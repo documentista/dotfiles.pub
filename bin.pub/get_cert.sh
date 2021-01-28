276839df6a2410329ea8a731daf432ac1068142c
Tom Dolan <thomas.dolan@libertymutual.com>
2021-01-28 16:56:28 -0500

    Fix sym linking of files in bin.pub

--- bin.pub/get_cert.sh
+++ bin.pub/get_cert.sh
@@ -2,7 +2,7 @@
 
 ## Script retrieves SSL certs for a url and sends them to stdout.
 ## First argument is the url and is mandatory
-## Second argument is port.  It is optional.  If a port is not supplied, port 443 is used.
+## Second argument is the port.  It is optional.  If a port is not supplied, port 443 is used.
 
 if [[ -z "$1" ]]; then
  echo "Please enter url"
